682f008071bc086a477dfde09d69a106134cdca6
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-03-14 17:17:51 +0300

    add: methods for adding and deleting notes

--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,6 @@ from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.contrib.auth import aauthenticate, alogin, logout
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-
 import json
 
 
@@ -31,7 +30,7 @@ async def add_user(request: HttpRequest):
         await alogin(request, user)
         return redirect('')
     except Exception as ex:
-        return JsonResponse({"error": f"{ex}"}, status=400)
+        return JsonResponse({"error": str(ex)}, status=400)
 
 
 
@@ -53,7 +52,7 @@ async def authorize(request: HttpRequest):
     except ValidationError:    
         return JsonResponse({"error": "invalid password or user"}, status=400)
     except Exception as ex:
-        return JsonResponse({"error": f"{ex}"}, status=400)
+        return JsonResponse({"error": str(ex)}, status=400)
 
 
 # logout method
@@ -62,4 +61,35 @@ def logout(request: HttpRequest):
     return redirect('login')
 
 
+#api method for creating notes
+@login_required
+@require_POST
+def create_note(request: HttpRequest):
+    try:
+        decoded = request.body.decode()
+        kwargs = json.loads(decoded)
+        name = kwargs["name"]
+        text = kwargs["text"]
+        user = request.user
+        note_repo = NoteRepo()
+        note_repo.create(name = name, text = text, user = user)
+        return JsonResponse({"error": "note added"}, status=200)
+    except Exception as ex:
+        return JsonResponse({"error": str(ex)}, status=400)
 
+#api method for deleting notes
+@login_required
+@require_POST
+def delete_note(request: HttpRequest):
+    try:
+        decoded = request.body.decode()
+        kwargs = json.loads(decoded)
+        note_repo = NoteRepo()
+        note = note_repo.get_by_id(kwargs["id"])
+        if note.user != request.user:
+            return JsonResponse({"error": "permission denied"}, status=403)
+
+        note_repo.delete_by_id(kwargs["id"])
+        return JsonResponse({"message": "note deleted"}, status=200)
+    except Exception as ex:
+        return JsonResponse({"error": str(ex)}, status=400)
