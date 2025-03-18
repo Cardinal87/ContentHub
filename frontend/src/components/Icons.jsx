1f010662e5403d72decc04c0bc18325ed337d2c1
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-03-18 20:42:15 +0300

    add: jsx file for svg icons

---
+++ frontend/src/components/Icons.jsx
@@ -0,0 +1,167 @@
+export function ProfileIcon({ classes }) {
+  return (
+    <svg
+      width="30"
+      height="30"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${classes}`}
+    >
+      {" "}
+      <path
+        d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
+        className="stroke-blue-600"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />{" "}
+      <path
+        d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457"
+        className="stroke-blue-600"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />{" "}
+      <path
+        d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z"
+        className="stroke-blue-600"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function SettingsIcon({ classes }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width={27}
+      height={27}
+      className={`fill-blue-600 ${classes}`}
+    >
+      <path
+        d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3
+          39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1
+          31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2
+            17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6
+            .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4
+              191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1
+              9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7
+                8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
+      />
+    </svg>
+  );
+}
+
+export function LeftArrow() {
+  return (
+    <svg
+      width="30px"
+      height="30px"
+      fill="none"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19 19L12.7071 12.7071C12.3166 12.3166 12.3166 11.6834 12.7071 11.2929L19 5"
+        className="stroke-blue-600"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11 19L4.70711 12.7071C4.31658 12.3166 4.31658 11.6834 4.70711 11.2929L11 5"
+        className="stroke-blue-600"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function RightArrow() {
+  return (
+    <svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.5 5L11.7929 11.2929C12.1834 11.6834 12.1834 12.3166 11.7929 12.7071L5.5 19"
+        className="stroke-blue-600"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M13.5 5L19.7929 11.2929C20.1834 11.6834 20.1834 12.3166 19.7929 12.7071L13.5 19"
+        className="stroke-blue-600"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function LogoutIcon({ classes }) {
+  return (
+    <svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${classes}`}
+    >
+      <path
+        d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.2429 22 18.8286 22 16.0002 22H15.0002C12.1718 22 10.7576 22 9.87889 21.1213C9.11051 20.3529 9.01406 19.175 9.00195 17"
+        className="stroke-blue-600"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15"
+        className="stroke-blue-600"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function DeleteIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="deletebtn"
+      className="fill-blue-600 hover:fill-blue-500 transition-colors duration-300 mr-1 ml-3"
+      width="25"
+      height="25"
+      viewBox="0 0 16 16"
+    >
+      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+    </svg>
+  );
+}
+
+export function EditIcon({ classes }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      id="changebtn"
+      className={`fill-blue-600 ${classes}`}
+      width="25"
+      height="25"
+      viewBox="0 0 16 16"
+    >
+      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+    </svg>
+  );
+}
--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  DeleteIcon,
+  EditIcon,
+  LeftArrow,
+  LogoutIcon,
+  ProfileIcon,
+  RightArrow,
+  SettingsIcon,
+} from "./Icons";
+
 function NoteText() {
   return (
-    <div className="container">
+    <div className="container h-[960px]">
       <p
         className="w-4xl h-[900px] break-words text-gray-100 overflow-x-hidden overflow-y-auto text-left
-                    bg-gray-700 border border-gray-500 rounded-xl shadow-xl p-3"
+                    bg-gray-600 border border-gray-500 rounded-xl shadow-xl p-3"
       >
         Text
       </p>
@@ -13,9 +24,9 @@ function NoteText() {
 
 function NoteEditing() {
   return (
-    <div className="container flex flex-col">
+    <div className="container flex flex-col h-[960px]">
       <textarea
-        className=" h-[900px] w-4xl text-gray-100 resize-none bg-gray-700 border border-gray-500
+        className=" h-[900px] w-4xl text-gray-100 resize-none bg-gray-700 border border-blue-300
                     rounded-xl shadow-xl p-3 transition-colors duration-300
                     outline-none focus:border-blue-700 hover:[&:not(:focus)]:border-blue-500"
         placeholder="your note"
@@ -39,59 +50,42 @@ function NoteEditing() {
 
 function DeleteButton() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      id="deletebtn"
-      className="fill-blue-600 hover:fill-blue-500 transition-colors duration-300 mr-1 ml-3"
-      width="25"
-      height="25"
-      viewBox="0 0 16 16"
-    >
-      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-    </svg>
+    <button className="border border-transparent rounded-lg transition-all duration-300">
+      <DeleteIcon />
+    </button>
   );
 }
 
-function ChangeButton() {
+function EditButton({ isEditing, setEditing }) {
+  const btnRef = useRef(null);
+  function onClick() {
+    setEditing((prev) => !prev);
+  }
+
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      id="changebtn"
-      className="fill-blue-600 hover:fill-blue-500 transition-colors duration-300"
-      width="25"
-      height="25"
-      viewBox="0 0 16 16"
+    <button
+      onClick={onClick}
+      ref={btnRef}
+      className={`border ${
+        isEditing ? "border-blue-600" : "border-transparent"
+      } rounded-lg p-1 transition-colors duration-300`}
     >
-      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-    </svg>
+      <EditIcon
+        classes={
+          "hover:fill-blue-500 transition-colors ease-in-out duration-200"
+        }
+      />
+    </button>
   );
 }
 
 function SettingsButton() {
   return (
     <button
-      className="flex text-gray-100 mx-3 text-xl items-center hover:bg-gray-700
+      className="flex text-gray-100 mx-3 text-0.5xl items-center hover:bg-gray-700
     py-2 rounded-lg px-1 transition-colors duration-300"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        width={27}
-        height={27}
-        className="fill-blue-600 mr-3"
-      >
-        <path
-          d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3
-          39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1
-          31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2
-            17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6
-            .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4
-              191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1
-              9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7
-                8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
-        />
-      </svg>
+      <SettingsIcon classes={"mr-3"}></SettingsIcon>
       Settings
     </button>
   );
@@ -100,38 +94,23 @@ function SettingsButton() {
 function ProfileButton() {
   return (
     <button
-      className="flex text-gray-100 mx-3 text-xl items-center hover:bg-gray-700 
-    py-2 px-1 rounded-lg transition-colors duration-300"
+      className={`flex text-gray-100 text-0.5xl items-center hover:bg-gray-700 
+    transition-colors duration-300 justify-start py-2 px-1 rounded-lg mx-3`}
     >
-      <svg
-        width="30"
-        height="30"
-        stroke-width="1.5"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        className="mr-3"
-      >
-        {" "}
-        <path
-          d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
-          className="stroke-blue-600"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />{" "}
-        <path
-          d="M4.271 18.3457C4.271 18.3457 6.50002 15.5 12 15.5C17.5 15.5 19.7291 18.3457 19.7291 18.3457"
-          className="stroke-blue-600"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />{" "}
-        <path
-          d="M12 12C13.6569 12 15 10.6569 15 9C15 7.34315 13.6569 6 12 6C10.3431 6 9 7.34315 9 9C9 10.6569 10.3431 12 12 12Z"
-          className="stroke-blue-600"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
+      <ProfileIcon classes={"mr-3"}></ProfileIcon>
       Profile
+    </button>
+  );
+}
+
+function LogoutButton() {
+  return (
+    <button
+      className="flex text-gray-100 text-0.5xl items-center hover:bg-gray-700 
+    transition-colors duration-300 justify-start py-2 pr-1 pl-0 rounded-lg mx-3"
+    >
+      <LogoutIcon classes={"mr-3"} />
+      Logout
     </button>
   );
 }
@@ -144,34 +123,83 @@ function Name({ name }) {
   );
 }
 
-function SideBar({ children }) {
-  return (
-    <aside
-      className="left-0 top-0 border-r-2 border-gray-500 bg-gray-900 fixed w-[250px] h-screen z-50
-    transition-transform duration-300 flex flex-col"
-    >
-      <div className="h-7/8">
-        <ul></ul>
-      </div>
-      <hr className="text-gray-100"></hr>
-      <div className="flex flex-col justify-end mt-auto mb-3">
-        <ProfileButton></ProfileButton>
-        <SettingsButton></SettingsButton>
-      </div>
-    </aside>
-  );
+function Sidebar({ children }) {
+  const [isOpened, setIsOpened] = useState(false);
+  function changeSidebarState() {
+    setIsOpened((prev) => !prev);
+  }
+
+  if (isOpened)
+    return (
+      <aside
+        className={`left-0 top-0 border-r-2 border-gray-500 bg-gray-900 fixed w-[250px] h-screen z-50
+      transition-transform duration-400 flex flex-col`}
+      >
+        <div className="flex h-1/11 items-start justify-end">
+          <button
+            className="rounded-4xl border-2 border-transparent hover:border-blue-600 
+          transition-colors duration-200 mt-4 mr-2"
+            onClick={changeSidebarState}
+          >
+            <LeftArrow></LeftArrow>
+          </button>
+        </div>
+        <hr className="text-gray-100"></hr>
+        <div className="h-8/10">
+          <ul></ul>
+        </div>
+        <hr className="text-gray-100"></hr>
+        <div className="flex flex-col mt-3 mb-3 items-stretch">
+          <ProfileButton />
+          <SettingsButton />
+          <LogoutButton />
+        </div>
+      </aside>
+    );
+  else {
+    return (
+      <aside
+        className={`left-0 top-0 border-r-2 border-gray-500 bg-gray-900 fixed w-[250px] h-screen z-50
+        transition-transform duration-400 flex flex-col -translate-x-49`}
+      >
+        <div className="flex h-1/11 items-start justify-end">
+          <button
+            className="rounded-4xl border-2 border-transparent hover:border-blue-600 
+          transition-colors duration-200 mt-4 mr-2"
+            onClick={changeSidebarState}
+          >
+            <RightArrow></RightArrow>
+          </button>
+        </div>
+        <hr className="text-gray-100"></hr>
+        <div className="h-8/10"></div>
+        <hr className="text-gray-100"></hr>
+        <div className="flex flex-col mt-auto mb-3 items-end">
+          <ProfileIcon classes={"mr-2"} />
+          <SettingsIcon classes={"mr-2.5 mt-2"} />
+          <button
+            className="border-2 border-transparent rounded-lg p-1 hover:border-blue-600 mr-2 mt-2
+          transition-colors duration-300 text-center"
+          >
+            <LogoutIcon />
+          </button>
+        </div>
+      </aside>
+    );
+  }
 }
 
 export function MainPage() {
+  const [isEditing, setEditing] = useState(false);
   return (
     <div className="container flex flex-col">
-      <SideBar></SideBar>
+      <Sidebar></Sidebar>
       <Name name={"note"}></Name>
       <div className="flex justify-end mb-3 mt-2">
-        <ChangeButton></ChangeButton>
+        <EditButton setEditing={setEditing} isEditing={isEditing}></EditButton>
         <DeleteButton></DeleteButton>
       </div>
-      <NoteEditing></NoteEditing>
+      {isEditing ? <NoteEditing></NoteEditing> : <NoteText></NoteText>}
     </div>
   );
 }
