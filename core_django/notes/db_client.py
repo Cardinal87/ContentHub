af581c0870103e4f2d6940c169127c9a2e222de0
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-03-27 21:20:25 +0300

    add: rag_serive and endpoints for saving and deleting vectors

--- core_django/notes/db_client.py
+++ core_django/notes/db_client.py
@@ -1,13 +1,14 @@
 from .models import Note
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from asgiref.sync import sync_to_async
 
 #CRUD operations for User
 class UserRepo():
     allowed_attrs = ["username", "password"]
     
-    def create(self, kwargs):
-        user = User.objects.create_user(
+    async def create(self, kwargs):
+        user = await sync_to_async(User.objects.create_user)(
             username=kwargs.get("username"),
             password=kwargs.get("password"),
         )
@@ -16,31 +17,32 @@ class UserRepo():
     def get_all(self):
         return User.objects.all()
     
-    def get_by_id(self, id:  int):
-        return User.objects.get(id=id)
+    async def get_by_id(self, id:  int):
+        return await User.objects.aget(id=id)
 
-    def get_by_name(self, name: str):
+    async def get_by_name(self, name: str):
         try:
-            return User.objects.get(username=name)
+            return await User.objects.aget(username=name)
         except User.DoesNotExist:
             raise ValidationError("incorrect username")
 
-    def delete_by_id(self, id: int):
-        User.objects.get(id=id).delete()
+    async def delete_by_id(self, id: int):
+        user = await User.objects.aget(id=id)
+        await user.adelete()
 
-    def update(self, user: User, kwargs):
+    async def update(self, user: User, kwargs):
         for key, value in kwargs.items():
             if key not in self.allowed_attrs:
                 raise ValidationError(f"{key} attribute is not allowed to change")
             setattr(user, key, value)
-        user.save()
+        await user.asave()
 
 
 #CRUD operations for Note
 class NoteRepo():
     allowed_attrs = ["text", "name"]
-    def create(self, **kwargs):
-        note = Note.objects.create(
+    async def create(self, **kwargs):
+        note = await Note.objects.acreate(
             name=kwargs.get("name"),
             text=kwargs.get("text"),
             user=kwargs.get("user")
@@ -50,18 +52,25 @@ class NoteRepo():
     def get_all(self):
         return Note.objects.all()
 
-    def get_by_id(self, id: int):
-        return Note.objects.get(id= id)
+    async def get_by_id(self, id: int):
+        return await Note.objects.aget(id= id)
 
     def get_user_notes(self, user: User):
         return Note.objects.filter(user=user)
     
-    def delete_by_id(self, id: int):
-        Note.objects.get(id=id).delete()
+    async def get_by_id_and_user(self, id: int, user: User):
+        try:
+            return await Note.objects.aget(id=id, user=user)
+        except:
+            raise ValidationError("Not found or permission denied")
 
-    def update(self, note: Note, **kwargs):
+    async def delete_by_id(self, id: int):
+        note = await Note.objects.aget(id=id)
+        await note.adelete()
+
+    async def update(self, note: Note, **kwargs):
         for key, value in kwargs.items():
             if key not in self.allowed_attrs:
                 raise ValueError(f"{key} attribute is not allowed to change")
             setattr(note, key, value)
-        note.save()
+        note.asave()
--- core_django/notes/views.py
+++ core_django/notes/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.http import HttpResponse, JsonResponse, HttpRequest
+import os
+import httpx
+from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import *  
 from .db_client import NoteRepo, UserRepo
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.exceptions import ValidationError
 from django.contrib.auth import aauthenticate, alogin, logout
-from django.shortcuts import redirect, render
 from django.db import IntegrityError
-from asgiref.sync import sync_to_async
 import json
+from asgiref.sync import sync_to_async
 
 #checking authorization
-def check_auth(request: HttpRequest):
+async def check_auth(request: HttpRequest):
     if request.user.is_authenticated:
         return JsonResponse({"message": "authenticated", "id": request.user.id}, status=200)
     else:
@@ -24,7 +23,7 @@ async def add_user(request: HttpRequest):
         decoded = request.body.decode()
         kwargs = json.loads(decoded)
         repo = UserRepo()
-        user = await sync_to_async(repo.create)(kwargs)
+        user = await repo.create(kwargs)
         await alogin(request, user)
         return JsonResponse({"message": "success"}, status=200)
     except IntegrityError as ex:
@@ -57,15 +56,16 @@ async def authorize(request: HttpRequest):
 
 # logout method
 @require_POST
-def logout_user(request: HttpRequest):
-    logout(request)
+async def logout_user(request: HttpRequest):
+    await sync_to_async(logout)(request)
     return JsonResponse({"message": "user loged out"}, status=200)
 
 
 #api method for creating notes
 @require_POST
-def create_note(request: HttpRequest):
+async def create_note(request: HttpRequest):
     try:
+        chat_url = os.getenv('AI_CHAT_URL')
         if not request.user.is_authenticated:
             return JsonResponse({"error": "unauthorized"}, status=401)
         decoded = request.body.decode()
@@ -74,43 +74,43 @@ def create_note(request: HttpRequest):
         text = kwargs["text"]
         user = request.user
         note_repo = NoteRepo()
-        new_note = note_repo.create(name = name, text = text, user = user)
+        new_note = await note_repo.create(name = name, text = text, user = user)
+
         return JsonResponse({"message": "note added", "id": new_note.id}, status=200)
     except Exception as ex:
         return JsonResponse({"error": str(ex)}, status=500)
 
 #api method for deleting notes
 @require_POST
-def delete_note(request: HttpRequest):
+async def delete_note(request: HttpRequest):
     try:
         if not request.user.is_authenticated:
             return JsonResponse({"error": "unauthorized"}, status=401)
         decoded = request.body.decode()
         kwargs = json.loads(decoded)
         note_repo = NoteRepo()
-        note = note_repo.get_by_id(kwargs["id"])
-        if note.user != request.user:
-            return JsonResponse({"error": "permission denied"}, status=403)
-
-        note_repo.delete_by_id(kwargs["id"])
+        note = await note_repo.get_by_id_and_user(kwargs["id"], user=request.user)
+        await note_repo.delete_by_id(kwargs["id"])
         return JsonResponse({"message": "note deleted"}, status=200)
+    except ValidationError as ex:
+        return JsonResponse({"error": "permission denied or not found"}, status=403)
     except Exception as ex:
         return JsonResponse({"error": str(ex)}, status=500)
     
 #api method for getting user notes
 @require_GET
-def get_notes(request: HttpRequest):
+async def get_notes(request: HttpRequest):
     try:
         if not request.user.is_authenticated:
             return JsonResponse({"error": "unauthorized"}, status=401)
         note_repo = NoteRepo()
-        notes = list(note_repo.get_user_notes(request.user).values())
+        notes = await sync_to_async(list)(note_repo.get_user_notes(request.user).values())
         return JsonResponse(notes, status=200, safe=False)
     except Exception as ex:
         return JsonResponse({"error": str(ex)}, status=500)
 
 @require_POST
-def update_note(request: HttpRequest):
+async def update_note(request: HttpRequest):
     try:
         if not request.user.is_authenticated:
             return JsonResponse({"error": "unauthorized"}, status=401)
@@ -119,11 +119,11 @@ def update_note(request: HttpRequest):
         kwargs = json.loads(decoded)
         name = kwargs["name"]
         text = kwargs["text"]
-        note = note_repo.get_by_id(kwargs["id"])
-        if note.user != request.user:
-            return JsonResponse({"error": "permission denied"}, status=403)
-        note_repo.update(note, name=name, text=text)
+        note = await note_repo.get_by_id_and_user(kwargs["id"], user=request.user)
+        await note_repo.update(note, name=name, text=text)
         return JsonResponse({"message": "note updated"}, status=200)
+    except ValidationError as ex:
+        return JsonResponse({"error": "permission denied or not found"}, status=403)
     except Exception as ex:
         return JsonResponse({"error": str(ex)}, status=500)
     
